4fd42013029c46c5616b6ec0936ebd0f32b4b1e7
jonathan.arroyave <jonathan.arroyave@grupokonecta.com>
2021-11-02 16:36:34 -0600

    Creacion config BD PHP
    Creacion config BD PHP

---
+++ config/bd.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=172.20.1.36;dbname=ci_monitoreov2',
+    //'dsn' => 'mysql:host=localhost;dbname=ci_monitoreov2',
+    'username' => 'ci_monitoreo',
+    'password' => 'monit000r300',
+    'charset' => 'utf8',
+
+    'on afterOpen' =>function ($event) {
+  	$event->sender->createCommand("SET lc_time_names = 'es_CO'")->execute();
+}
+];
