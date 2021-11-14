fbe2f969aeb5aba728de549e607398cccda3e9b7
Andersson Moreno <anmorenoa@grupokonecta.com>
2021-11-14 17:55:12 -0500

    TemaBuzonesCXMParte6
    Se coloca la condicional para que traigan todos los buzones vacios en Normal y Aleatorio en sus tipo de inbox

--- controllers/BasesatisfaccionController.php
+++ controllers/BasesatisfaccionController.php
@@ -3859,9 +3859,15 @@ where tbl_segundo_calificador.id_ejecucion_formulario = tbl_ejecucionformularios
                     $afecha = date('Y-m-d H:i:s', $afecha);
                 }
 
-                $allModels = BaseSatisfaccion::find()
-                            ->where('buzon = ""')
+                if ($aleatorio == "1") {
+                    $allModels = BaseSatisfaccion::find()->where(['tipo_inbox' => 'ALEATORIO'])
+                            ->andWhere('(buzon = "")')
                             ->andWhere('fecha_satu BETWEEN "' . $afecha . '" AND "' . $fecha . '"');
+                } else {
+                    $allModels = BaseSatisfaccion::find()->where(['tipo_inbox' => 'NORMAL'])
+                            ->andWhere('(buzon = "")')
+                            ->andWhere('fecha_satu BETWEEN "' . $afecha . '" AND "' . $fecha . '"');
+                }
 
                 if ($arregloFiltro['pcrc'] != '') {
                     $allModels->andFilterWhere([
