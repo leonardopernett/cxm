aef15283f30f1e5dfa9d8e521ac8ea91e85399b9
sary sanabria valencia <sary.sanabria@grupokonecta.com>
2021-11-23 14:47:18 -0500

    Librerias
    Se agrega exporting modules

--- views/basechat/importarexcel.php
+++ views/basechat/importarexcel.php
@@ -91,8 +91,8 @@ $this->params['breadcrumbs'][] = $this->title;
 </style>
 <link rel="stylesheet" href="../../css/font-awesome/css/font-awesome.css"  >
 <script src="../../web/js_extensions/jquery-2.1.1.min.js"></script>
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="../../web/js_extensions/highcharts/exporting.js"></script>
+<script src="../../web/js_extensions/highcharts/modules_exporting.js"></script>
 <!-- Full Page Image Header with Vertically Centered Content -->
 <header class="masthead">
   <div class="container h-100">
