014b93d7f1301962b839046219f9c8f1709a37ae
Andersson Moreno <anmorenoa@grupokonecta.com>
2022-01-27 12:06:54 -0500

    AgregarProcesosFocalizado
    Proceos de escucha focalizado

--- views/dashboardspeechdos/indexvoice.php
+++ views/dashboardspeechdos/indexvoice.php
@@ -1984,7 +1984,7 @@ $varColorM = null;
     
     <div class="col-md-2">
       <div class="card1 mb">
-        <label style="font-size: 15px;"><em class="fas fa-phone-square" style="font-size: 15px; color: #FFC72C;"></em> Análisis Focalizada: </label>
+        <label style="font-size: 15px;"><em class="fas fa-phone-square" style="font-size: 15px; color: #FFC72C;"></em> Análisis Focalizado: </label>
         <?= Html::a('Interacciones',  ['searchllamadas', 'varprograma'=>$varNamePCRC, 'varcodigopcrc'=>$txtCodPcrcok, 'varidcategoria'=>$txtIdCatagoria1, 'varextension'=>$txtParametros, 'varfechasinicio'=>$varInicioF, 'varfechasfin'=>$varFinF, 'varcantllamadas'=>$txtTotalLlamadas, 'varfechainireal'=>$txtFechaIni, 'varfechafinreal'=>$txtFechaFin,'varcodigos'=>$varCodigo,'varaleatorios' => 0], ['class' => 'btn btn-success',
                           'style' => 'background-color: #337ab7', 'target' => "_blank",
                           'data-toggle' => 'tooltip',
