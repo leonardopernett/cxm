b951f16e1e401649bcabdc8771c1f23f6dd7e8de
Andersson Moreno <anmorenoa@grupokonecta.com>
2022-02-01 13:50:31 -0500

    ProcesosValoracionFechaExtendida
    Procesos de fecha extendida a la valoracion

--- views/seguimientoplan/gestionarescalamientos.php
+++ views/seguimientoplan/gestionarescalamientos.php
@@ -33,8 +33,10 @@ $month = date('m');
 $year = date('Y');
 $day = date("d", mktime(0,0,0, $month+1, 0, $year));
      
-$varfechainicio = date('Y-m-d', mktime(0,0,0, $month, 1, $year));
-$varfechafin = date('Y-m-d', mktime(0,0,0, $month, $day, $year));
+// $varfechainicio = date('Y-m-d', mktime(0,0,0, $month, 1, $year));
+$varfechainicio = '2022-01-01';
+// $varfechafin = date('Y-m-d', mktime(0,0,0, $month, $day, $year));
+$varfechafin = "2022-02-05";
 
 ?>
 <style>
