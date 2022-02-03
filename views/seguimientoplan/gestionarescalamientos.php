a4e64751c098abcca33b045d7a0f448665950330
Andersson Moreno <anmorenoa@grupokonecta.com>
2022-02-03 16:30:07 -0500

    ProcesosCambiantesValoraciones
    Se realia el proceso para generar las acciones de valoracion mes actual

--- views/seguimientoplan/gestionarescalamientos.php
+++ views/seguimientoplan/gestionarescalamientos.php
@@ -33,10 +33,9 @@ $month = date('m');
 $year = date('Y');
 $day = date("d", mktime(0,0,0, $month+1, 0, $year));
      
-// $varfechainicio = date('Y-m-d', mktime(0,0,0, $month, 1, $year));
-$varfechainicio = '2022-01-01';
-// $varfechafin = date('Y-m-d', mktime(0,0,0, $month, $day, $year));
-$varfechafin = "2022-02-05";
+$varfechainicio = date('Y-m-d', mktime(0,0,0, $month, 1, $year));
+$varfechafin = date('Y-m-d', mktime(0,0,0, $month, $day, $year));
+
 
 ?>
 <style>
