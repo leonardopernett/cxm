7a586dcfeb39a10a4041b816f2376a06de5c3018
Juan Pablo Garzon <juan.garzon@grupokonecta.com>
2022-02-21 15:28:08 -0500

    codigo-limpio-config
    codigo-limpio-config

--- config/dbjarvis.php
+++ config/dbjarvis.php
@@ -1,11 +1,6 @@
 <?php
 
 return [
-   // 'class'        => 'yii\db\Connection',
-   // 'dsn'          => 'mysql:host=172.102.180.196;port=3306;dbname=jarvis',
-   // 'username'     => 'EXP_CONSULTA',
-   // 'password'     => 'Konecta2020*',
-   // 'charset'      => 'utf8',
 
    'class'        => 'yii\db\Connection',
    'dsn'          => 'mysql:host='.MY_SQL_HOST_JARVIS.';port='.MY_SQL_PORT_JARVIS.';dbname='.MY_SQL_DB_NAME_JARVIS,
--- config/dbjarvis2.php
+++ config/dbjarvis2.php
@@ -7,12 +7,6 @@ return [
    'password'     => PASSWORD_DB_JARVIS,
    'charset'      => 'utf8',
 
-   // 'class'        => 'yii\db\Connection',
-   // 'dsn'          => 'mysql:host=172.102.180.196;port=3306;dbname=jarvis',
-   // 'username'     => 'EXP_CONSULTA',
-   // 'password'     => 'Konecta2020*',
-   // 'charset'      => 'utf8',
-
 
    'on afterOpen' => function ($event) {
        $event->sender->createCommand("SET lc_time_names = 'es_CO'")->execute();
