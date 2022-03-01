bac7e203d8ca3f4f31d0b5a6a3398e54c772eeba
Andersson Moreno <anmorenoa@grupokonecta.com>
2022-03-01 10:41:36 -0500

    ProcesosMesActual
    Procesos de mes actual

--- views/controlprocesos/index.php
+++ views/controlprocesos/index.php
@@ -340,7 +340,7 @@ $this->params['breadcrumbs'][] = $this->title;
                                                                 # code...
                                                                 break;
                                                         } 
-                                    if ($model->tipo_corte == "Corte ".$txtMes." - General Konecta" || $model->tipo_corte == "Corte ".$txtMes." - Grupo Bancolombia" || $model->tipo_corte == "Corte ".$txtMes." - Directv" || $model->tipo_corte == "Corte ".$txtMes." - Nutresa") {                          
+                                    // if ($model->tipo_corte == "Corte ".$txtMes." - General Konecta" || $model->tipo_corte == "Corte ".$txtMes." - Grupo Bancolombia" || $model->tipo_corte == "Corte ".$txtMes." - Directv" || $model->tipo_corte == "Corte ".$txtMes." - Nutresa") {                          
                                             
                                             return Html::a('<span class="glyphicon glyphicon-pencil"></span>',['update2', 'id' => $model->id, 'evaluados_id' => $model->evaluados_id], [
                                                 'class' => '',
@@ -348,7 +348,7 @@ $this->params['breadcrumbs'][] = $this->title;
                                                     'method' => 'post',
                                                 ],
                                             ]);
-                                    }
+                                    // }
                                 
                                  },
                                          'delete' => function($url, $model){
