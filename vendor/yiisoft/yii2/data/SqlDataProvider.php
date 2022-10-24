b778215baa0a81e3ad932b0be1572f568750ac75
Andersson Moreno <anmorenoa@grupokonecta.com>
2022-10-24 09:30:31 -0500

    CambioSqlDataProvider
    Se cambia conexion al SQL Data Provider

--- vendor/yiisoft/yii2/data/SqlDataProvider.php
+++ vendor/yiisoft/yii2/data/SqlDataProvider.php
@@ -67,7 +67,7 @@ class SqlDataProvider extends BaseDataProvider
      * @var Connection|array|string the DB connection object or the application component ID of the DB connection.
      * Starting from version 2.0.2, this can also be a configuration array for creating the object.
      */
-    public $db = 'dbslave';
+    public $db = 'db';
     /**
      * @var string the SQL statement to be used for fetching data rows.
      */
