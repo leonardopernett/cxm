50c0d89403b678805c2ff272743022808d25ebc3
Andersson Moreno <anmorenoa@grupokonecta.com>
2022-11-18 15:14:45 -0500

    EnvioAlertas
    Se procesa el envio de las alertas

--- controllers/ProcesosadministradorController.php
+++ controllers/ProcesosadministradorController.php
@@ -1335,6 +1335,7 @@ use app\models\Encuestaspersonalsatu;
                     'usua_id' => Yii::$app->user->identity->id,                      
                 ])->execute();
 
+                $tmpFile = "images/Alertas_Satu.jpg";
                 
                 $message = "<html><body>";
                 $message .= "<h3>CX-MANAGEMENT</h3>";
@@ -1345,6 +1346,7 @@ use app\models\Encuestaspersonalsatu;
                     ->setTo($varObtenerCorreo)
                     ->setFrom(Yii::$app->params['email_satu_from'])
                     ->setSubject("Informe Encuestas de Satisfacción - CX-MANAGEMENT")
+                    ->attach($tmpFile)
                     ->setHtmlBody($message)
                     ->send();
 
@@ -1379,15 +1381,17 @@ use app\models\Encuestaspersonalsatu;
                     'usua_id' => Yii::$app->user->identity->id,                      
             ])->execute();
 
+            $tmpFile = "images/Alertas_Satu.jpg";
+
             $message = "<html><body>";
             $message .= "<h3>CX-MANAGEMENT</h3>";
-            $message .= "<img src="'https://qa.grupokonecta.local/qa_managementv2/web/images/Alertas_Satu.jpg'" border="'1'">";
             $message .= "</body></html>";
 
             Yii::$app->mailer->compose()
                     ->setTo($varObtenerCorreoMasivo)
                     ->setFrom(Yii::$app->params['email_satu_from'])
                     ->setSubject("Informe Encuestas de Satisfacción - CX-MANAGEMENT")
+                    ->attach($tmpFile)
                     ->setHtmlBody($message)
                     ->send();
 
