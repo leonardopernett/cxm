1a91021635bdf937cb7f619db361982d426f36a4
Andersson Moreno <anmorenoa@grupokonecta.com>
2022-11-18 16:10:43 -0500

    ProcesosCorresoAlertas
    Se genera cambios

--- controllers/ProcesosadministradorController.php
+++ controllers/ProcesosadministradorController.php
@@ -1335,17 +1335,17 @@ use app\models\Encuestaspersonalsatu;
                     'usua_id' => Yii::$app->user->identity->id,                      
                 ])->execute();
 
-                
+                $tmpFile = "images/Alertas_Satu.jpg";
                 
                 $message = "<html><body>";
-                $message .= "<h3>CX-MANAGEMENT</h3>";
-                $message .= "<img src="'https://qa.grupokonecta.local/qa_managementv2/web/images/Alertas_Satu.jpg'" border="'1'">";
+                $message .= "<h3>CX-MANAGEMENT</h3>";                
                 $message .= "</body></html>";
 
                 Yii::$app->mailer->compose()
                     ->setTo($varObtenerCorreo)
                     ->setFrom(Yii::$app->params['email_satu_from'])
-                    ->setSubject("Informe Encuestas de Satisfacción - CX-MANAGEMENT")
+                    ->setSubject("Informe Encuestas de Satisfacción - CX-MANAGEMENT")                    
+                    ->attach($tmpFile)
                     ->setHtmlBody($message)
                     ->send();
 
@@ -1380,15 +1380,17 @@ use app\models\Encuestaspersonalsatu;
                     'usua_id' => Yii::$app->user->identity->id,                      
             ])->execute();
 
+            $tmpFile = "images/Alertas_Satu.jpg";
+
             $message = "<html><body>";
             $message .= "<h3>CX-MANAGEMENT</h3>";
-            $message .= "<img src="'https://qa.grupokonecta.local/qa_managementv2/web/images/Alertas_Satu.jpg'" border="'1'">";
             $message .= "</body></html>";
 
             Yii::$app->mailer->compose()
                     ->setTo($varObtenerCorreoMasivo)
                     ->setFrom(Yii::$app->params['email_satu_from'])
                     ->setSubject("Informe Encuestas de Satisfacción - CX-MANAGEMENT")
+                    ->attach($tmpFile)
                     ->setHtmlBody($message)
                     ->send();
 
