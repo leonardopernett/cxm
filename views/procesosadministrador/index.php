94d3495fa4704d6b82239276a4da32f01410b0e7
Diego Montoya <diego.montoya@grupokonecta.com>
2023-02-22 08:34:40 -0500

    modificacionmoduloparametrizador
    Se agregan los modelos al modulo parametrizador

--- views/procesosadministrador/index.php
+++ views/procesosadministrador/index.php
@@ -324,6 +324,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     </div>
                 </div>
             </div>
+            <hr>
             <div class="row">
                 <div class="col-md-4">
                     <div class="card1 mb">
@@ -353,6 +354,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     </div>
                 </div>
             </div>
+            <hr>
             <div class="row">
                 <div class="col-md-4">
                     <div class="card1 mb">
