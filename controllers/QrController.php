6d04265ab229f9cc04c3fd7fd63dc346646b9cdc
Andersson Moreno <anmorenoa@grupokonecta.com>
2023-03-21 14:41:30 -0500

    PruebasCorreos_QR
    se genera pruebas de QR

--- controllers/QrController.php
+++ controllers/QrController.php
@@ -662,6 +662,27 @@ public function actionVeranexometri($id){
   ]);
 }
 
+    public function actionPruebasenvio(){
+
+      $tmpFile = 'images/uploads/qyr_1679417953_Carta respuesta Q&R procesada.pdf';  
+
+                $message = "<html><body>";
+                $message .= "<h3>CX-MANAGEMENT</h3>";   
+                $message .= "Hola, Te enviamos la respuesta de tu caso.";
+                $message .= "<br> Gracias por tu espera, tenemos respuesta a tu caso. Esperamos tu revisión y aceptación de la misma.";             
+                $message .= "</body></html>";
+
+                Yii::$app->mailer->compose()
+                    ->setTo('diego.montoya@grupokonecta.com','anmorenoa@grupokonecta.com')
+                    ->setFrom(Yii::$app->params['email_satu_from'])
+                    ->setSubject("Actualización de tu caso QyR - CX-MANAGEMENT")                    
+                    ->attach($tmpFile)
+                    ->setHtmlBody($message)
+                    ->send();
+
+      return $this->render('index');
+    }
+
   }
 
 ?>
