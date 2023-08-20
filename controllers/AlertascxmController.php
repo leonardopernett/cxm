41101c9058c6c52e0210eb2964c59ee73c78bfcc
Andersson Moreno <anmorenoa@grupokonecta.com>
2023-08-20 18:28:30 -0500

    CambiosTamañoLetra
    Se genera el cambio del tamaño de la letra para el envio del correo

--- controllers/AlertascxmController.php
+++ controllers/AlertascxmController.php
@@ -371,9 +371,9 @@ use Exception;
             <body class='text-center'>
                 <h1><label style='font-family: sans-serif; color: #1d2d4f;'>Informe de Alertas CX-Management</label></h1>
                 <hr>
-                <h5><label style='font-family: sans-serif;'>Actualmente se tiene una alerta que fue realizada desde CX-Management. Para validar la alerta te recomendamos ingresar al módulo de reporte alertas de CXM y buscarlo para ver resultados.</a></label></h5>
+                <h4><label style='font-family: sans-serif;'>Actualmente se tiene una alerta que fue realizada desde CX-Management. Para validar la alerta te recomendamos ingresar al módulo de reporte alertas de CXM y buscarlo para ver resultados.</a></label></h4>
                 <br>
-                <h5><label style='font-family: sans-serif;'>¡Hola equipo! Te comentamos que nos encantaria saber tú opinión, por eso te invitamos a ingresar a CXM y responder la encuesta en el siguiente link <a href='https://qa.grupokonecta.local/qa_managementv2/web/index.php/alertascxm/alertaencuesta?id_alerta=".$varIdAlertas."'>Ingresar a la encuesta</a></label></h5>
+                <h4><label style='font-family: sans-serif;'>¡Hola equipo! Te comentamos que nos encantaria saber tú opinión, por eso te invitamos a ingresar a CXM y responder la encuesta en el siguiente link <a href='https://qa.grupokonecta.local/qa_managementv2/web/index.php/alertascxm/alertaencuesta?id_alerta=".$varIdAlertas."'>Ingresar a la encuesta</a></label></h4>
                 <hr>
                 <h7><label style='font-family: sans-serif;'>© CX-Management 2023 - Desarrollado por Konecta</a></label></h>
 
